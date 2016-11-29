55f19957406f554db837b01308711e03589f4478
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-29 18:31:56 +0100

    Fix bug in urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from .views import BookInventoryListView
 
+
+app_name = 'inventory'
+
 urlpatterns = [
     url(r'^(?P<category_num>)\d*/?$', BookInventoryListView.as_view(), name='bookpage'),
 ]
