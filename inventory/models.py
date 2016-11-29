a0fc3d979ebe9157e929ed0991787cb1cf6ea74f
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-29 16:48:22 +0100

    Implement Category model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Implementation of Category model."""
+
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
