b073507b62ba19994f28124e0d014de295724553
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-29 19:11:52 +0100

    Configure DEBUG setting to be set by env variable

--- bookkeeper/settings.py
+++ bookkeeper/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = dotenv.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = dotenv.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
