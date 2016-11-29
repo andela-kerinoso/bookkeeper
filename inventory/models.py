82c0559a96063b95928f6b4547b127b8efa8c5d4
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-29 17:01:15 +0100

    Implement Book model

--- inventory/models.py
+++ inventory/models.py
@@ -8,3 +8,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    """Implementation of Book model."""
+
+    title = models.CharField(max_length=255)
+    authors = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        unique_together = ('title', 'authors')
