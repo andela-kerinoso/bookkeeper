75aa6caa6139c824f148dddbfb323d544f65ea18
andela-kerinoso <kolawole.erinoso@andela.com>
2016-11-29 16:46:08 +0100

    Test Category model

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,16 @@
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoryTest(TestCase):
+    """Test for Category model."""
+
+    def setUp(self):
+        self.category = Category.objects.create(name='Biology')
+
+    def test_uniqueness_of_category_name(self):
+        category = Category(name='Biology')
+
+        self.assertRaises(IntegrityError, category.save)
